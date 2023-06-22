269389c0e6617307f11a59c8894524d4a5510f5a
나데다 <75760843+nadeda@users.noreply.github.com>
2023-06-22 17:10:42 +0900

    MOD added idea files in gitignore

--- src/main/java/com/woojinens/SpringBootWebApplication.java
+++ src/main/java/com/woojinens/SpringBootWebApplication.java
@@ -1,9 +1,9 @@
-package com.mkyong;
+package com.woojinens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringBootWebApplication extends SpringBootServletInitializer {
--- src/main/java/com/woojinens/WelcomeController.java
+++ src/main/java/com/woojinens/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.mkyong;
+package com.woojinens;
 
 import java.util.Map;
 
